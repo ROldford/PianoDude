2195ae1dc526058220df94c93c6f88cf0458823b
Unknown <unknown@example.com>
2020-02-01 20:58:58 -0800

    implemented piano notes

--- src/main/Sound/PianoNote.java
+++ src/main/Sound/PianoNote.java
@@ -1,41 +1,20 @@
 package Sound;
 
-import jm.music.data.Note;
-import jm.util.Play;
+import org.jfugue.player.Player;
 
 public class PianoNote {
 
-    public void PianoNote (String name) {
-        Note note = new Note();
+    Player player;
 
-        if (name.equals("a")) {
-            note.setPitch(57);
-            Play.midi(note);
-        }
-        if (name.equals("b")) {
-            note.setPitch(59);
-            Play.midi(note);
-        }
-        if (name.equals("c")) {
-            note.setPitch(60);
-            Play.midi(note);
-        }
-        if (name.equals("d")) {
-            note.setPitch(62);
-            Play.midi(note);
-        }
-        if (name.equals("e")) {
-            note.setPitch(64);
-            Play.midi(note);
-        }
-        if (name.equals("f")) {
-            note.setPitch(65);
-            Play.midi(note);
-        }
-        if (name.equals("g")) {
-            note.setPitch(67);
-            Play.midi(note);
-        }
-
+    public PianoNote(){
+        player=new Player();
     }
+
+    public void PlayNote(String note) {
+        player.play(note);
+    }
+
 }
+
+
+
