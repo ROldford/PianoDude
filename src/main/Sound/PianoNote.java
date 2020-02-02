916c26dfdaf431a110e6339a21bc908461c9cccc
Sydney <smschmid@ualberta.ca>
2020-02-01 17:11:38 -0800

    Problems with wrapping PianoNote type around Note type

--- src/main/Sound/PianoNote.java
+++ src/main/Sound/PianoNote.java
@@ -11,62 +11,80 @@ public class PianoNote {
     Note noteE;
     Note noteF;
     Note noteG;
+    Note note;
 
     private String name;
     private int pitch;
     private int volume;
-    private int noteLength;
+    private int length;
+
+
+    public PianoNote() {
+        note = new Note();
+    }
+
+//    public Note makeNote(String name, int pitch){
+//        this.name = name;
+//        this.pitch = note.setPitch(pitch);
+//        note.setDynamic(volume);
+//        note.setDuration(length);
+//        Play.midi(note);
+//    }
 
     public void makeNoteA(){
         noteA = new Note();
         this.name = "a";
         noteA.setPitch(57);
-        noteA.setDynamic(5);
-        noteA.setDuration(1);
-    }
-
-    public void makeNoteB(){
-        noteA = new Note();
-        this.name = "b";
-        noteA.setPitch(59);
-        noteA.setDynamic(5);
-        noteA.setDuration(1);
-    }
-
-    public void makeNoteC(){
-        noteA = new Note();
-        this.name = "c";
-        noteA.setPitch(60);
-        noteA.setDynamic(5);
-        noteA.setDuration(1);
-    }
-
-    public void makeNoteD(){
-        noteA = new Note();
-        this.name = "d";
-        noteA.setPitch(62);
-        noteA.setDynamic(5);
-        noteA.setDuration(1);
-    }
-
-    public void makeNoteF(){
-        noteA = new Note();
-        this.name = "f";
-        noteA.setPitch(65);
-        noteA.setDynamic(5);
-        noteA.setDuration(1);
-    }
-    public void makeNoteG(){
-        noteA = new Note();
-        this.name = "g";
-        noteA.setPitch(67);
-        noteA.setDynamic(5);
-        noteA.setDuration(1);
-    }
-
-    public void play(){
+        noteA.setDynamic(volume);
+        noteA.setDuration(length);
         Play.midi(noteA);
     }
 
+//    public void makeNoteB(){
+//        noteB = new Note();
+//        this.name = "b";
+//        noteB.setPitch(59);
+//        noteB.setDynamic(volume);
+//        noteB.setDuration(length);
+//    }
+//
+//    public void makeNoteC(){
+//        noteC = new Note();
+//        this.name = "c";
+//        noteC.setPitch(60);
+//        noteC.setDynamic(volume);
+//        noteC.setDuration(length);
+//    }
+//
+//    public void makeNoteD(){
+//        noteD = new Note();
+//        this.name = "d";
+//        noteD.setPitch(62);
+//        noteD.setDynamic(volume);
+//        noteD.setDuration(length);
+//    }
+//
+//    public void makeNoteE(){
+//        noteE = new Note();
+//        this.name = "e";
+//        noteE.setPitch(64);
+//        noteE.setDynamic(volume);
+//        noteE.setDuration(length);
+//    }
+//
+//    public void makeNoteF(){
+//        noteF = new Note();
+//        this.name = "f";
+//        noteF.setPitch(65);
+//        noteF.setDynamic(volume);
+//        noteF.setDuration(length);
+//    }
+//    public void makeNoteG(){
+//        noteG = new Note();
+//        this.name = "g";
+//        noteG.setPitch(67);
+//        noteG.setDynamic(volume);
+//        noteG.setDuration(length);
+//    }
 
 }
--- src/test/Sound/testPianoNote.java
+++ src/test/Sound/testPianoNote.java
@@ -1,14 +1,54 @@
 package Sound;
 
-import
+import jm.music.data.Note;
+import jm.util.Play;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import Sound.PianoNote;
 
 public class testPianoNote {
-    Note note;
+    private static final int NOTE_A = 57;
+    private static final int NOTE_B = 59;
+    private static final int NOTE_C = 60;
+    private static final int NOTE_D = 62;
+    private static final int NOTE_E = 64;
+    private static final int NOTE_F = 65;
+    private static final int NOTE_G = 67;
+    private PianoNote note;
+    private PianoNote noteA;
+
+    private String name;
+    private int pitch;
+    private int volume;
+    private int length;
 
     @BeforeEach
     private void runBefore(){
+        note = new PianoNote();
+    }
 
+//    @Test
+//    private Note testMakeNote(){
+//        assertEquals(NOTE_A, );
+//        makeNoteA("a", NOTE_A);
+//        Play.midi();
+//    }
+
+    @Test
+    private void testMakeNoteA(){
+        note.makeNoteA();
+        assertEquals(NOTE_A,);
+    }
+
+    @Test
+    public void makeNoteA(){
+        noteA = new Note();
+        this.name = "a";
+        noteA.setPitch(57);
+        noteA.setDynamic(volume);
+        noteA.setDuration(length);
+        Play.midi(noteA);
     }
 
 }
